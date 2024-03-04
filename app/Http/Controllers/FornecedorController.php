c48b8e5395a2f40f23cf81b21c38f240940ef004
Kevin Costa Oliveira <kevin_costa47@hotmail.com>
2024-03-04 13:50:52 -0300

    atualizado

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,22 +7,12 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = ['Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                         'Fornecedor 1',
-                        ];
+        $fornecedores = [0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' =>'0'],
+                         1 => ['nome' => 'Fornecedor 2', 'status' => 'S']];
+
+        // echo $msg = isset ($fornecedores[0]['cnpj']) ? 'CNPJ informado' : "CNPJ não informado" ;
+
+
         return view ('app.fornecedor.index',compact('fornecedores'));
     }
 }
--- resources/views/app/Fornecedor/index.blade.php
+++ resources/views/app/Fornecedor/index.blade.php
@@ -2,9 +2,10 @@
 
 {{-- Fica o comentario que será descartado pelo interpretador do blade --}}
 
+{{-- 
 {{'Texto teste '}}
 
-<?= 'Texto teste '?>
+<?= 'Texto teste '?> --}}
 
 @php
 
@@ -13,9 +14,7 @@
 //   else{}
 
 @endphp
-
-
-
+{{-- 
 @if(count($fornecedores) > 0 && count($fornecedores) < 10) {
     <h3> Existem alguns fornecedores cadastrados </h3>
 }
@@ -26,6 +25,57 @@
 {
     <h3> ainda não existem fornecedores cadastrados</h3>
 }
-@endif
+@endif --}}
 
-@dd($fornecedores)
+
+    {{-- @dd($fornecedores) --}}
+
+    {{-- @unless executa se a condição for falsa --}}
+
+{{-- @isset ($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset ($fornecedores[0]['cnpj'])
+        Cnpj: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
+    <br>
+    <br>
+    @if(!($fornecedores[0]['status'] == 'S'))
+        Fornecedor inativo
+    @endif
+    <br>
+    @unless ($fornecedores[0]['status'] == 'S')
+            Fornecedor inativo
+    @endunless 
+    <br>    
+@endisset --}}
+
+@php
+    /*
+    if(empty($variavel)) {} //retornar true se a varivel estiver vazia)
+     - ''
+     - 0
+     - 0.0
+     - '0'
+     - null
+     - false
+     - array()
+     - $var
+    */
+@endphp
+
+@isset($fornecedores)
+    Fornecedor: {{$fornecedores[1] ['nome']}}
+    <br>
+    Status: {{$fornecedores[1] ['status']}}
+    <br>
+    Cnpj: {{$fornecedores[1] ['cnpj'] ?? 'Dado não foi preenchido'}}
+    {{-- se a variavel testada não estiver definida 
+        ou
+        $variavel testada possuir o valor null --}}
+@endisset
